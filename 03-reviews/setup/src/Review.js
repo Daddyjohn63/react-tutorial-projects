507e4fd45df10a42e239eebd92a64213c8031231
john paul <john@webandprosper.co.uk>
2022-12-14 08:50:49 +0000

    Final review project

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,68 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
-
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
-  return <h2>review component</h2>;
+  // setup state to create an index for the array that is coming in. We set zero as the array is zero index based.
+  const [index, setIndex] = useState(0);
+  //get the properties and pass in the index value.
+  const { name, job, image, text } = people[index];
+
+  //checkNumber is a function we can use to ensure we never go beyond the lower and upper bands of the array size. When we call the event listeners, we will use this function to return the number which can be used in state to determin the array index and hence the data passing in ... simples.
+  const checkNumber = number => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+  const nextPerson = () => {
+    setIndex(index => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex(index => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  //set random person functionality.
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
+    </article>
+  );
 };
 
 export default Review;
